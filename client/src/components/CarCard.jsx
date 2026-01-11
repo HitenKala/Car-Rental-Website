10e769299d448eab0fbd6e306a62dcd20325a183
HitenKala <hitenkala1@gmail.com>
2026-01-11 12:48:35 +0530

    CarCard modified

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -12,7 +12,7 @@ const CarCard = ({car}) => {
         <div className='relative h-48 overflow-hidden'>
             <img src={car.image} alt="Car Image" className='w-full h-full object-cover-transition-transform duration-500 group-hover:scale-105'/>
 
-            {car.isAvaliable && <p className='absolute top-4 left-4 bg-[#185a9d] text-white text-xs px-2.5 py-1 rounded-full'>Available now</p>}
+            {car.isAvailable && <p className='absolute top-4 left-4 bg-[#185a9d] text-white text-xs px-2.5 py-1 rounded-full'>Available now</p>}
             
             <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm-text-white px-3 py-2 rounded-lg'>
             <span className='font-semibold text-white/80'>{currency}{car.pricePerDay}</span>
