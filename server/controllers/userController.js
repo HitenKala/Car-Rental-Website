e5ff5aeec01e8d2eab074d9a9aadb77c38e75563
HitenKala <hitenkala1@gmail.com>
2026-01-22 22:46:06 +0530

    user authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js"
-import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
 //Generate JWT Token
@@ -14,20 +14,20 @@ export const regsisterUser = async(req, res) => {
         const {name, email, password} = req.body
 
         if (!name || !email || !password || password.length<8){
-            return res.json({sucess:false, message:"Fill all the fields correctly"})
+            return res.json({success:false, message:"Fill all the fields correctly"})
         }
         const userExists = await User.findOne({email})
         if (userExists){
-            return res.json({sucess:false, message:"User already exists"})
+            return res.json({success:false, message:"User already exists"})
         }
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({name,email,password:hashedPassword})
         const token = generateToken(user._id.toString())
-        res.json({sucess: true, token})
+        res.json({success: true, token})
 
     } catch (error) {
         console.log(error.message);
-        res.json({sucess:false, message:error.message})
+        res.json({success:false, message:error.message})
     }
 }
 
@@ -37,17 +37,31 @@ export const loginUser = async(req, res) => {
         const {email, password} = req.body
         const user = await User.findOne({email})
         if (!user){
-            return res.json({sucess:false, message:"User does not exist"})
+            return res.json({success:false, message:"User does not exist"})
         }
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch){
-            return res.json({sucess:false, message:"Invalid credentials"})
+            return res.json({success:false, message:"Invalid credentials"})
         }
         const token = generateToken(user._id.toString())
-        res.json({sucess: true, token})
+        res.json({success: true, token})
 
     } catch (error) {
         console.log(error.message);
-        res.json({sucess:false, message:error.message})
+        res.json({success:false, message:error.message})
+    }
+}
+
+//Get User data from token(JWT)
+export const getUserData = async(req,res)=>{
+    try {
+        const {user} = req;
+        if (!user) {
+            return res.json({success:false, message:"User not found"})
+        }
+        res.json({success:true, user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
     }
 }
