ffa6efafcd46ea74d969d022edd78c5590c9e8d7
HitenKala <hitenkala1@gmail.com>
2026-02-06 22:17:24 +0530

    refactor: enhance Hero component with motion effects for improved animations

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { assets, cityList } from '../assets/assets'
 import { useAppContext } from '../context/AppContext';
+import { motion } from 'motion/react';
 
 const Hero = () => {
 
@@ -13,16 +14,28 @@ const Hero = () => {
   }
 
   return (
-    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-neutral2 text-center' >
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      className='h-screen flex flex-col items-center justify-center gap-14 bg-neutral2 text-center' >
 
-      <h1 className='text-4xl md:text-5xl font-semibold'>Cars On Rent</h1>
+      <motion.h1 initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        className='text-4xl md:text-5xl font-semibold'>Cars On Rent</motion.h1>
 
-      <form onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg
+      <motion.form
+        initial={{ scale: 0.95, opacity: 0, y: 50 }}
+        animate={{ scale: 1, opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+
+        onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg
          md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]' action="">
 
         <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
           <div className='flex flex-col items-start gap-2'>
-            <select value={pickupLocation} onChange={e=>setPickupLocation(e.target.value)} id="pickup-location" required>
+            <select value={pickupLocation} onChange={e => setPickupLocation(e.target.value)} id="pickup-location" required>
               <option value="" disabled selected>Pickup Location</option>
               {cityList.map((city) => <option key={city} value={city}>{city}</option>)}
             </select>
@@ -30,26 +43,34 @@ const Hero = () => {
 
           <div className='flex flex-col items-start gap-2'>
             <label htmlFor="pickup-date">Pick-up Date</label>
-            <input value={pickupDate} onChange={e=>setPickupDate(e.target.value)} type="date" id='pickup-date' min={new Date().toISOString().split('T')[0]}
+            <input value={pickupDate} onChange={e => setPickupDate(e.target.value)} type="date" id='pickup-date' min={new Date().toISOString().split('T')[0]}
               className='text-sm text-gary-500' required />
           </div>
           <div className='flex flex-col items-start gap-2'>
             <label htmlFor="return-date">Return Date</label>
-            <input value={returnDate} onChange={e=>setReturnDate(e.target.value)} type="date" id='return-date'
+            <input value={returnDate} onChange={e => setReturnDate(e.target.value)} type="date" id='return-date'
               className='text-sm text-gary-500' required />
           </div>
-          <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-[#185a9d] hover:bg-neutral3 text-white rounded-full cursor-pointer'>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-[#185a9d] hover:bg-neutral3 text-white rounded-full cursor-pointer'>
             <img src={assets.search_icon} alt="search" className='border-white brightness-300' />
             Search
-          </button>
+          </motion.button>
 
         </div>
 
-      </form>
+      </motion.form>
 
-      <img src={assets.main_car4} alt="Car" className='max-h-74 w-220' />
+      <motion.img
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.8, delay: 0.6 }}
 
-    </div>
+      src={assets.main_car4} alt="Car" className='max-h-74 w-220' />
+
+    </motion.div>
   )
 }
 
