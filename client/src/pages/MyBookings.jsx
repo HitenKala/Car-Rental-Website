f7d44ef56444bd9150289a5545212e3e934d51bc
HitenKala <hitenkala1@gmail.com>
2026-02-13 23:30:10 +0530

    Project Completed

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 import Title from '../components/Title'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react';
 
 const MyBookings = () => {
 
@@ -28,7 +29,12 @@ const MyBookings = () => {
   }, [user])
 
   return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm  max-w-7xl'>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+
+      className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm  max-w-7xl'>
 
       <Title title='My Bookings'
         subTitle='View and manage your car bookings'
@@ -36,7 +42,12 @@ const MyBookings = () => {
 
       <div>
         {bookings.map((booking, index) => (
-          <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 border border-gray-200 rounded-lg p-6 mt-5 first:mt-12'>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.1, duration: 0.4 }}
+
+            key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 border border-gray-200 rounded-lg p-6 mt-5 first:mt-12'>
             {/* Car Image + Info */}
 
             <div className='md:col-span-1'>
@@ -81,12 +92,12 @@ const MyBookings = () => {
             </div>
 
 
-          </div>
+          </motion.div>
         ))}
       </div>
 
 
-    </div>
+    </motion.div>
   )
 }
 
